1c14b845484fc293560c7e39d42208e338f31d12
dosh <974378025@qq.com>
2019-12-20 01:50:35 +0800

    django自带的form表单：django.form.Form

--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
-
+from . import models
+from . import forms
 # Create your views here.
 
 
@@ -9,7 +10,28 @@ def index(request):
 
 
 def login(request):
-    pass
+    if request.method == "POST":
+        login_form = forms.UserForm(request.POST)
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        message = '请检查填写的内容！'
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
+            try:
+                user = models.User.objects.get(name=username)
+            except:
+                message = '用户不存在'
+                return render(request,'login/login.html',locals())
+
+            if user.password == password:
+                return redirect('/index')
+            else:
+                message = '密码不正确！'
+                return render(request,'login/login.html',locals())
+        else:
+            return request(request,'login/login.html',locals())
+    login_form = forms.UserForm()
     return render(request,'login/login.html')
 
 
@@ -20,4 +42,5 @@ def register(request):
 
 def logout(request):
     pass
+
     return redirect("/login/")
