b2a5fffc8ddb7290b4710f07e685062bf36d3f53
dosh <974378025@qq.com>
2019-12-24 01:12:42 +0800

    captcha commit

--- login/views.py
+++ login/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render,redirect
+# login/views.py
+
+from django.shortcuts import render
+from django.shortcuts import redirect
 from . import models
 from . import forms
 # Create your views here.
@@ -6,41 +9,40 @@ from . import forms
 
 def index(request):
     pass
-    return render(request,'login/index.html')
+    return render(request, 'login/index.html')
 
 
 def login(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         login_form = forms.UserForm(request.POST)
-        username = request.POST.get('username')
-        password = request.POST.get('password')
         message = '请检查填写的内容！'
         if login_form.is_valid():
             username = login_form.cleaned_data.get('username')
             password = login_form.cleaned_data.get('password')
+
             try:
                 user = models.User.objects.get(name=username)
-            except:
-                message = '用户不存在'
-                return render(request,'login/login.html',locals())
+            except :
+                message = '用户不存在！'
+                return render(request, 'login/login.html', locals())
 
             if user.password == password:
-                return redirect('/index')
+                return redirect('/index/')
             else:
                 message = '密码不正确！'
-                return render(request,'login/login.html',locals())
+                return render(request, 'login/login.html', locals())
         else:
-            return request(request,'login/login.html',locals())
+            return render(request, 'login/login.html', locals())
+
     login_form = forms.UserForm()
-    return render(request,'login/login.html')
+    return render(request, 'login/login.html', locals())
 
 
 def register(request):
     pass
-    return render(request,'login/register.html')
+    return render(request, 'login/register.html')
 
 
 def logout(request):
     pass
-
     return redirect("/login/")
